84fc4808799d1cd4893aec1c43f6fb7a7eb5f30d
Forselo <129178216+Forselo@users.noreply.github.com>
2023-04-03 08:57:42 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,6 +1,7 @@
 package guru.qa;
 
 public class Main {
+    private static String msg = ""Hello qa.ru!!!!!"";
     public static void main(String[] args) {
         System.out.println("Hello qa.quru!!!!!!");
     }
