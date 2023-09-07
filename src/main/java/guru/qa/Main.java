be30deb882086aae7bce53fd435c47b994f43b57
kadyrov <ilnur.kadyrov@ligastavok.ru>
2023-09-07 10:40:26 +0300

    add getMessage() method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,9 +2,13 @@ package guru.qa;
 
 
 public class Main {
-    private static String msg = "Hello qagu.ru!!!!!";
+
     public static void main(String[] args) {
         System.out.println(getMessage());
     }
 
+    private static String getMessage(){
+        return "Hello qagu.ru";
+
+ }
 }
